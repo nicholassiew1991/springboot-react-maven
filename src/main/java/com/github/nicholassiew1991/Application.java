a6bf46af007d6820e855254a232666b13dcb22aa
nicholassiew1991 <nicholas_siew1991@hotmail.com>
2022-05-27 18:24:18 +0800

    Initialise Project

---
+++ src/main/java/com/github/nicholassiew1991/Application.java
@@ -0,0 +1,11 @@
+package com.github.nicholassiew1991;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+}
